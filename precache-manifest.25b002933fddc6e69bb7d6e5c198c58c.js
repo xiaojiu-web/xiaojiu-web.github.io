e7eecad163fc42473b909b05cd5dee0775b70dbb
xiaojiu-web <271853345@qq.com>
2020-04-12 00:04:45 +0800

    简博客

---
+++ precache-manifest.25b002933fddc6e69bb7d6e5c198c58c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af90cc6a9210fb5d8355569bf50bee5",
+    "url": "index.html"
+  },
+  {
+    "revision": "06cd8e6a19115c54826b6707cca26e85",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a6ff8ac881684f66a04b",
+    "url": "static/css/app.ed7cf676.css"
+  },
+  {
+    "revision": "4ead520a1c93288b2fa2",
+    "url": "static/css/chunk-vendors.c995535d.css"
+  },
+  {
+    "revision": "671cadec1ea080693c2137144d288d9c",
+    "url": "static/fonts/iconfont.671cadec.eot"
+  },
+  {
+    "revision": "88f7674a13398fdb67416866a668788a",
+    "url": "static/fonts/iconfont.88f7674a.ttf"
+  },
+  {
+    "revision": "98fcb2226a543cd6115af5c587740847",
+    "url": "static/fonts/iconfont.98fcb222.woff"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "static/img/iconfont.d41d8cd9.svg"
+  },
+  {
+    "revision": "a6ff8ac881684f66a04b",
+    "url": "static/js/app.094fc1c4.js"
+  },
+  {
+    "revision": "4ead520a1c93288b2fa2",
+    "url": "static/js/chunk-vendors.ea49d04f.js"
+  }
+]);
